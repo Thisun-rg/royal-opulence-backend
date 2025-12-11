a8bae07b846141415e0e565d380c10b11894ca2e
Sanduni Jayasinghe <sandunilakshika008@gmail.com>
2025-12-11 11:27:25 +0530

    Add Invoice DTOs

---
+++ src/main/java/com/royalopulence/dto/payment/InvoiceRequest.java
@@ -0,0 +1,23 @@
+package com.royalopulence.dto.payment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class InvoiceRequest {
+
+    @NotBlank(message = "Reservation ID is required")
+    private String reservationId;
+
+    @NotBlank(message = "Payment ID is required")
+    private String paymentId;
+
+    @NotNull(message = "Total amount is required")
+    @Positive(message = "Total amount must be positive")
+    private Double totalAmount;
+
+    private String currency;
+}
+
