41367930f31eb72c433043edc4ddc9c7ed4e7408
Sanduni Jayasinghe <sandunilakshika008@gmail.com>
2025-12-11 11:46:11 +0530

    Add PaymentService DTOs

---
+++ src/main/java/com/royalopulence/service/base/PaymentService.java
@@ -0,0 +1,21 @@
+package com.royalopulence.service.base;
+
+import com.royalopulence.dto.payment.PaymentRequest;
+import com.royalopulence.dto.payment.PaymentResponse;
+
+import java.util.List;
+
+public interface PaymentService {
+
+    PaymentResponse createPayment(PaymentRequest request);
+
+    PaymentResponse getPaymentById(String id);
+
+    List<PaymentResponse> getAllPayments();
+
+    List<PaymentResponse> getPaymentsByReservationId(String reservationId);
+
+    PaymentResponse markPaymentSuccess(String id);
+
+    PaymentResponse markPaymentFailed(String id);
+}
