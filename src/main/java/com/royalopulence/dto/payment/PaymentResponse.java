cd59e098d657c4c332e4a0f3292ba57b0d396a2e
Sanduni Jayasinghe <sandunilakshika008@gmail.com>
2025-12-11 11:22:21 +0530

    Add PaymentResponse DTO

---
+++ src/main/java/com/royalopulence/dto/payment/PaymentResponse.java
@@ -0,0 +1,17 @@
+package com.royalopulence.dto.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PaymentResponse {
+    private String id;
+    private String reservationId;
+    private Double amount;
+    private String currency;
+    private String status;        // PENDING, SUCCESS, FAILED
+    private String method;        // NOT_SET, STRIPE, PAYHERE (later)
+    private Long createdAt;       // timestamp (ms)
+}
+
