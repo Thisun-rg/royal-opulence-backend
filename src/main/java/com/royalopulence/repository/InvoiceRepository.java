8c6e5414c7eb5ab00862a5ea65173a5647358c42
Sanduni Jayasinghe <sandunilakshika008@gmail.com>
2025-12-13 08:04:58 +0530

    Update InvoiceRepository with queries for paymentId and reservationId

--- src/main/java/com/royalopulence/repository/InvoiceRepository.java
+++ src/main/java/com/royalopulence/repository/InvoiceRepository.java
@@ -3,6 +3,14 @@ package com.royalopulence.repository;
 import com.royalopulence.model.operation.Invoice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface InvoiceRepository extends MongoRepository<Invoice, String> {
-}
+import java.util.List;
+import java.util.Optional;
 
+public interface InvoiceRepository extends MongoRepository<Invoice, String> {
+
+    // Get invoice by Payment ID
+    Optional<Invoice> findByPaymentId(String paymentId);
+
+    // Get all invoices by Reservation ID
+    List<Invoice> findByReservationId(String reservationId);
+}
