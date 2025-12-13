72ef4e98d402f827311392a756f21a0885512f05
Sanduni Jayasinghe <sandunilakshika008@gmail.com>
2025-12-13 21:46:39 +0530

    Add Stripe test-mode payment service

--- src/main/java/com/royalopulence/service/payment/StripePaymentService.java
+++ src/main/java/com/royalopulence/service/payment/StripePaymentService.java
@@ -1,16 +1,23 @@
+package com.royalopulence.service.payment;
+
+import com.stripe.Stripe;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.stereotype.Service;
+
 @Service
 public class StripePaymentService {
 
     public StripePaymentService() {
-        Stripe.apiKey = "sk_test_xxxxx";
+        Stripe.apiKey = "sk_test_REPLACE_WITH_TEST_KEY";
     }
 
     public String createPaymentIntent(double amount) throws Exception {
         PaymentIntent intent = PaymentIntent.create(
-            PaymentIntentCreateParams.builder()
-                .setAmount((long) (amount * 100))
-                .setCurrency("usd")
-                .build()
+                PaymentIntentCreateParams.builder()
+                        .setAmount((long) (amount * 100)) // cents
+                        .setCurrency("usd")
+                        .build()
         );
         return intent.getId();
     }
