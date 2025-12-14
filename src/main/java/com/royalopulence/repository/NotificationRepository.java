560ef19dfd46a497d9013df0e61a08bf4a1f12a3
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-14 20:15:47 +0530

    Notification Repository

--- src/main/java/com/royalopulence/repository/NotificationRepository.java
+++ src/main/java/com/royalopulence/repository/NotificationRepository.java
@@ -1,5 +1,12 @@
 package com.royalopulence.repository;
 
-public class NotificationRepository {
+import com.royalopulence.model.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByUserId(String userId);
 }
