0cf744b886ad182c84d3d88de3390d5ae9bac0c6
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-14 21:22:29 +0530

    Notification Controller class

--- src/main/java/com/royalopulence/controller/NotificationController.java
+++ src/main/java/com/royalopulence/controller/NotificationController.java
@@ -1,5 +1,32 @@
 package com.royalopulence.controller;
 
+import com.royalopulence.dto.NotificationRequest;
+import com.royalopulence.dto.NotificationResponse;
+import com.royalopulence.service.base.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notification")
+@RequiredArgsConstructor
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
+    @PostMapping
+    public NotificationResponse createNotification(@RequestBody NotificationRequest request) {
+        return notificationService.createNotification(request);
+    }
+
+    @GetMapping
+    public List<NotificationResponse> getAllNotifications() {
+        return notificationService.getAllNotifications();
+    }
+
+    @GetMapping("/{userId}")
+    public List<NotificationResponse> getUserNotifications(@PathVariable String userId) {
+        return notificationService.getUserNotifications(userId);
+    }
 }
