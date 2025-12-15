d61d7ef02fca43b3d3d3b18aa36eed9a7a50532e
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-15 17:40:24 +0530

    Promotion repository Updated-User Specific

--- src/main/java/com/royalopulence/repository/PromotionRepository.java
+++ src/main/java/com/royalopulence/repository/PromotionRepository.java
@@ -4,6 +4,11 @@ import com.royalopulence.model.core.Promotion;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PromotionRepository extends MongoRepository<Promotion, String> {
+
+    // user-specific + general promotions
+    List<Promotion> findByTargetUserIdOrTargetUserIdIsNull(String userId);
 }
