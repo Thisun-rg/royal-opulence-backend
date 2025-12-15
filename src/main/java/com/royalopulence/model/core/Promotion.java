075361f6c4e05fab9c3f3c622f7221b243f67420
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-15 11:13:22 +0530

    Promotion.java class-Added some codes

--- src/main/java/com/royalopulence/model/core/Promotion.java
+++ src/main/java/com/royalopulence/model/core/Promotion.java
@@ -1,7 +1,32 @@
 package com.royalopulence.model.core;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "promotions")
 public class Promotion {
 
-   
-    
+    @Id
+    private String id;
+
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private boolean active;
+
+    public Promotion() {
+        this.active = true;
+    }
+
+    public Promotion(String title, String description, Date startDate, Date endDate, boolean active) {
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.active = active;
+    }
+
 }
