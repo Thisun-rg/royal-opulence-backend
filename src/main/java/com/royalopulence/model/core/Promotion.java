796ab7732d5cf664eafb03ed7789aae305d76876
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-15 11:14:41 +0530

    Promotion.java class-Update Codes

--- src/main/java/com/royalopulence/model/core/Promotion.java
+++ src/main/java/com/royalopulence/model/core/Promotion.java
@@ -28,5 +28,16 @@ public class Promotion {
         this.endDate = endDate;
         this.active = active;
     }
+     public String getId() { return id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public Date getStartDate() { return startDate; }
+    public void setStartDate(Date startDate) { this.startDate = startDate; }
+    public Date getEndDate() { return endDate; }
+    public void setEndDate(Date endDate) { this.endDate = endDate; }
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
 
 }
