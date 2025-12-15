733a03c179458a787d111256acd898083126d50f
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-15 13:19:50 +0530

    Promotion Service class - Update Codes

--- src/main/java/com/royalopulence/service/base/PromotionService.java
+++ src/main/java/com/royalopulence/service/base/PromotionService.java
@@ -1,5 +1,15 @@
+
 package com.royalopulence.service.base;
 
-public class PromotionService {
+import com.royalopulence.dto.PromotionRequest;
+import com.royalopulence.dto.PromotionResponse;
 
+import java.util.List;
+
+public interface PromotionService {
+    PromotionResponse createPromotion(PromotionRequest request);
+
+    List<PromotionResponse> getAllPromotions();
+
+    PromotionResponse getPromotionById(String id);
 }
