2f9c9b7881718c97972d02249dd1c6a7518f1b30
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-15 11:22:37 +0530

    Promotion Repository class - Update Codes

--- src/main/java/com/royalopulence/repository/PromotionRepository.java
+++ src/main/java/com/royalopulence/repository/PromotionRepository.java
@@ -4,4 +4,6 @@ import com.royalopulence.model.core.Promotion;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
+public interface PromotionRepository extends MongoRepository<Promotion, String> {
+}
