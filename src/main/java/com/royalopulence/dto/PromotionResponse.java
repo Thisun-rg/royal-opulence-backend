935a17129bc6adce1a00473f8fb88ac92232a1ed
IBWalisinghe <ibwalis-ps21232@stu.kln.ac.lk>
2025-12-16 20:33:03 +0530

    Promotion Module Updated-automatically managed using expiry date logic

--- src/main/java/com/royalopulence/dto/PromotionResponse.java
+++ src/main/java/com/royalopulence/dto/PromotionResponse.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 @Data
 public class PromotionResponse {
 
-    private String id;
+    private String id; 
     private String title;
     private String description;
     private int discount;
---
+++ src/main/java/com/royalopulence/model/core/PromotionStatus.java
@@ -0,0 +1,7 @@
+package com.royalopulence.model.core;
+
+public enum PromotionStatus {
+    ACTIVE,
+    EXPIRED
+}
+
